5140fc6b059960a5ba7797d79e7c8ecb069502cc
John Waczak <john.louis.waczak@gmail.com>
2022-04-27 16:40:39 -0500

    another update to make the graphs prettier

--- dev/L63_EKF.jl
+++ dev/L63_EKF.jl
@@ -152,6 +152,25 @@ for k ∈ 1:nt
 end
 
 
-p = plot(t, uTrue[1,:], label="Truth")
+px = plot(t, uTrue[1,:], label="truth")
+plot!(px, t, ub[1,:], label="forecast")
+plot!(px, t_m, w[1,:], seriestype=:scatter, label="observation" )
+plot!(px, t, ua[1,:], label="analysis")
+xlabel!(px, "t")
+ylabel!(px, "x(t)")
 
+py = plot(t, uTrue[2,:], label="truth")
+plot!(py, t, ub[2,:], label="forecast")
+plot!(py, t_m, w[2,:], seriestype=:scatter, label="observation" )
+plot!(py, t, ua[2,:], label="analysis")
+xlabel!(py, "t")
+ylabel!(py, "y(t)")
 
+pz = plot(t, uTrue[3,:], label="truth")
+plot!(pz, t, ub[3,:], label="forecast")
+plot!(pz, t_m, w[3,:], seriestype=:scatter, label="observation" )
+plot!(pz, t, ua[3,:], label="analysis")
+xlabel!(pz, "t")
+ylabel!(pz, "z(t)")
+
+plot(px, py, pz, layout=(3,1))
